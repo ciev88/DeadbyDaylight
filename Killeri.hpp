359343ea3c21c4a7d26919e3d8c8c2db3dacebe7
ciev88 <ogi@tmina.org>
2020-05-22 19:38:23 +0200

    Revert "Revert "cek cek""
    This reverts commit 326c2892f364eab88a5dafa6618d4f60735a74a3.

---
+++ Killeri.hpp
@@ -0,0 +1,43 @@
+#ifndef KILLERI_HPP_INCLUDED
+#define KILLERI_HPP_INCLUDED
+#include "Item.hpp"
+#include "Igrac.hpp"
+
+class Killer:public Igrac
+{
+protected:
+    string Ime;
+    bool DLC;
+    KillerItem item;
+public:
+
+
+        Killer(string I, bool D, bool po, string n, string o, string v, bool os, double ms):Igrac(po), item(n, o, v, os, ms)
+
+        {
+            Ime=I;
+            DLC=D;
+        }
+        
+    friend ostream& operator<<(ostream& izlaz, const  Killer &k)
+    {
+
+        izlaz<<"Killer: ispis"<<endl;
+        izlaz<<"Ime: "<<k.Ime<<endl;
+        izlaz<<"DLC: "<<k.DLC<<endl;
+        izlaz<<k.item<<endl<<endl;
+
+        return izlaz;
+    }
+    
+
+    void Povezan()
+    {
+    	Igrac::Povezan();
+    	cout<<"Killer je online"<<endl;
+    }
+
+};
+
+
+#endif // KILLERI_HPP_INCLUDED
